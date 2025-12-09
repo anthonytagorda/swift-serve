3dad0b9a521c9dcd7a2465934715c26ad4ffe2e4
Anthony Tagorda <anthtago@gmail.com>
2025-12-10 07:41:59 +0800

    added store.js for redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
+import customerReducer from "./slices/customerSlice";
 
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    customer: customerReducer,
+  },
+  devTools: import.meta.env.MODE !== "production",
 });
 
 export default store;
