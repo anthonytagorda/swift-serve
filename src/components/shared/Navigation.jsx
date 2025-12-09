f33ab473ee76c058f53cac226d8d12b8b1558a3a
Anthony Tagorda <anthtago@gmail.com>
2025-12-10 05:12:40 +0800

    updated Navigation active tabs

--- src/components/shared/Navigation.jsx
+++ src/components/shared/Navigation.jsx
@@ -1,13 +1,14 @@
 import Modal from './Modal'
-import { FaHome } from 'react-icons/fa'
 import { MdOutlineReorder, MdTableBar } from 'react-icons/md'
 import { CiCircleMore } from 'react-icons/ci'
 import { BiSolidDish } from 'react-icons/bi'
-import { useNavigate } from 'react-router-dom'
+import { FaHome } from 'react-icons/fa'
+import { useLocation, useNavigate } from 'react-router-dom'
 import { useState } from 'react'
 
 const Navigation = () => {
   const navigate = useNavigate();
+  const location = useLocation();
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [guestCount, setGuestCount] = useState(0);
   const openModal = () => setIsModalOpen(true);
@@ -21,20 +22,29 @@ const Navigation = () => {
     if (guestCount <= 0) return;
     setGuestCount((prev) => prev - 1);
   }
-  const guestLabel = guestCount <= 1 ? "person" : "persons";
 
+  const guestLabel = guestCount <= 1 ? "person" : "persons";
+  const isActive = (path) => location.pathname === path
   return (
     <>
       <div className='fixed bottom-0 left-0 right-0 bg-white/90 backdrop-blur-sm shadow-[0_-8px_30px_rgba(0,0,0,0.08)] p-2 h-16 flex justify-around z-40'>
         <button
           onClick={() => navigate("/")}
-          className='flex items-center justify-center text-[#9D5623] bg-gray-100 w-[300px] rounded-[20px] cursor-pointer'>
+          className={`flex items-center justify-center ${isActive("/") ? "text-[#9D5623] bg-gray-100" : "text-[#ababab]"
+            } w-[300px] rounded-[20px] cursor-pointer`}
+        >
           <FaHome className="inline mr-2" size={20} /><p>Home</p>
         </button>
-        <button onClick={() => navigate("/orders")} className='flex items-center justify-center text-[#9D5623] w-[300px] cursor-pointer' >
+        <button
+          onClick={() => navigate("/orders")}
+          className={`flex items-center justify-center ${isActive("/orders") ? "text-[#9D5623]" : "text-[#ababab]"
+            } w-[300px] rounded-[20px] cursor-pointer`}>
           <MdOutlineReorder className="inline mr-2" size={20} /><p>Orders</p>
         </button>
-        <button onClick={() => navigate("/tables")} className='flex items-center justify-center text-[#9D5623] w-[300px] cursor-pointer'>
+        <button
+          onClick={() => navigate("/tables")}
+          className={`flex items-center justify-center ${isActive("/tables") ? "text-[#9D5623]" : "text-[#ababab]"
+            } w-[300px] rounded-[20px] cursor-pointer`}>
           <MdTableBar className="inline mr-2" size={20} /><p>Tables</p>
         </button>
         <button className='flex items-center justify-center text-[#9D5623] w-[300px] cursor-pointer'>
