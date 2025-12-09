678f5b636fb7bd11054415c10cdd05fb2a798173
Anthony Tagorda <anthtago@gmail.com>
2025-12-09 16:12:21 +0800

    added all dishes

--- src/constants/index.js
+++ src/constants/index.js
@@ -90,6 +90,281 @@ export const tables = [
   { id: 15, name: "Table 15", status: "Booked", initial: "TW", seats: 3 },
 ];
 
+export const startersItem = [
+  {
+    id: 1,
+    name: "Paneer Tikka",
+    price: 250,
+    category: "Vegetarian",
+  },
+  {
+    id: 2,
+    name: "Chicken Tikka",
+    price: 300,
+    category: "Non-Vegetarian",
+  },
+  {
+    id: 3,
+    name: "Tandoori Chicken",
+    price: 350,
+    category: "Non-Vegetarian",
+  },
+  {
+    id: 4,
+    name: "Samosa",
+    price: 100,
+    category: "Vegetarian",
+  },
+  {
+    id: 5,
+    name: "Aloo Tikki",
+    price: 120,
+    category: "Vegetarian",
+  },
+  {
+    id: 6,
+    name: "Hara Bhara Kebab",
+    price: 220,
+    category: "Vegetarian",
+  },
+];
+
+export const mainCourse = [
+  {
+    id: 1,
+    name: "Butter Chicken",
+    price: 400,
+    category: "Non-Vegetarian",
+  },
+  {
+    id: 2,
+    name: "Paneer Butter Masala",
+    price: 350,
+    category: "Vegetarian",
+  },
+  {
+    id: 3,
+    name: "Chicken Biryani",
+    price: 450,
+    category: "Non-Vegetarian",
+  },
+  {
+    id: 4,
+    name: "Dal Makhani",
+    price: 180,
+    category: "Vegetarian",
+  },
+  {
+    id: 5,
+    name: "Kadai Paneer",
+    price: 300,
+    category: "Vegetarian",
+  },
+  {
+    id: 6,
+    name: "Rogan Josh",
+    price: 500,
+    category: "Non-Vegetarian",
+  },
+];
+
+export const beverages = [
+  {
+    id: 1,
+    name: "Masala Chai",
+    price: 50,
+    category: "Hot",
+  },
+  {
+    id: 2,
+    name: "Lemon Soda",
+    price: 80,
+    category: "Cold",
+  },
+  {
+    id: 3,
+    name: "Mango Lassi",
+    price: 120,
+    category: "Cold",
+  },
+  {
+    id: 4,
+    name: "Cold Coffee",
+    price: 150,
+    category: "Cold",
+  },
+  {
+    id: 5,
+    name: "Fresh Lime Water",
+    price: 60,
+    category: "Cold",
+  },
+  {
+    id: 6,
+    name: "Iced Tea",
+    price: 100,
+    category: "Cold",
+  },
+];
+
+export const soups = [
+  {
+    id: 1,
+    name: "Tomato Soup",
+    price: 120,
+    category: "Vegetarian",
+  },
+  {
+    id: 2,
+    name: "Sweet Corn Soup",
+    price: 130,
+    category: "Vegetarian",
+  },
+  {
+    id: 3,
+    name: "Hot & Sour Soup",
+    price: 140,
+    category: "Vegetarian",
+  },
+  {
+    id: 4,
+    name: "Chicken Clear Soup",
+    price: 160,
+    category: "Non-Vegetarian",
+  },
+  {
+    id: 5,
+    name: "Mushroom Soup",
+    price: 150,
+    category: "Vegetarian",
+  },
+  {
+    id: 6,
+    name: "Lemon Coriander Soup",
+    price: 110,
+    category: "Vegetarian",
+  },
+];
+
+export const desserts = [
+  {
+    id: 1,
+    name: "Gulab Jamun",
+    price: 100,
+    category: "Vegetarian",
+  },
+  {
+    id: 2,
+    name: "Kulfi",
+    price: 150,
+    category: "Vegetarian",
+  },
+  {
+    id: 3,
+    name: "Chocolate Lava Cake",
+    price: 250,
+    category: "Vegetarian",
+  },
+  {
+    id: 4,
+    name: "Ras Malai",
+    price: 180,
+    category: "Vegetarian",
+  },
+];
+
+export const pizzas = [
+  {
+    id: 1,
+    name: "Margherita Pizza",
+    price: 350,
+    category: "Vegetarian",
+  },
+  {
+    id: 2,
+    name: "Veg Supreme Pizza",
+    price: 400,
+    category: "Vegetarian",
+  },
+  {
+    id: 3,
+    name: "Pepperoni Pizza",
+    price: 450,
+    category: "Non-Vegetarian",
+  },
+];
+
+export const alcoholicDrinks = [
+  {
+    id: 1,
+    name: "Beer",
+    price: 200,
+    category: "Alcoholic",
+  },
+  {
+    id: 2,
+    name: "Whiskey",
+    price: 500,
+    category: "Alcoholic",
+  },
+  {
+    id: 3,
+    name: "Vodka",
+    price: 450,
+    category: "Alcoholic",
+  },
+  {
+    id: 4,
+    name: "Rum",
+    price: 350,
+    category: "Alcoholic",
+  },
+  {
+    id: 5,
+    name: "Tequila",
+    price: 600,
+    category: "Alcoholic",
+  },
+  {
+    id: 6,
+    name: "Cocktail",
+    price: 400,
+    category: "Alcoholic",
+  },
+];
+
+export const salads = [
+  {
+    id: 1,
+    name: "Caesar Salad",
+    price: 200,
+    category: "Vegetarian",
+  },
+  {
+    id: 2,
+    name: "Greek Salad",
+    price: 250,
+    category: "Vegetarian",
+  },
+  {
+    id: 3,
+    name: "Fruit Salad",
+    price: 150,
+    category: "Vegetarian",
+  },
+  {
+    id: 4,
+    name: "Chicken Salad",
+    price: 300,
+    category: "Non-Vegetarian",
+  },
+  {
+    id: 5,
+    name: "Tuna Salad",
+    price: 350,
+  },
+];
+
 export const menus = [
   {
     id: 1,
