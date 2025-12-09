1e318fbc0e904d1c6a9543a89ca66fb3c6bdbec7
Anthony Tagorda <anthtago@gmail.com>
2025-12-09 18:03:04 +0800

    added Menu component

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import { Home, Auth, Orders, Tables } from "./pages"
+import { Home, Auth, Orders, Tables, Menu } from "./pages"
 import Header from "./components/shared/Header"
 
 function App() {
   return (
     <>
-     <Router>
-      <Header/>
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/auth" element={<Auth/>}/>
-        <Route path="/orders" element={<Orders/>}/>
-        <Route path="/tables" element={<Tables/>}/>
-      </Routes>
-     </Router>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/auth" element={<Auth />} />
+          <Route path="/orders" element={<Orders />} />
+          <Route path="/tables" element={<Tables />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="*" element={<div>Not Found</div>} />
+        </Routes>
+      </Router>
     </>
   )
 }
