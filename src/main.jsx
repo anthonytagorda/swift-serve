ab66ffa1a8ccb46b4fcee90ea5790b196acfb72c
Anthony Tagorda <anthtago@gmail.com>
2025-12-10 05:13:07 +0800

    added main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { StrictMode } from "react";
+import { Provider } from 'react-dom/client';
+import { createRoot } from "react-dom/client";
 import App from './App.jsx'
+import store from "./redux/store.js";
+import './index.css'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </StrictMode>
 )
