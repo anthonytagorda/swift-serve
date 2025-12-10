9061694daa2c1e68ae679ee2332a3545aaecffe7
anthonytagorda <anthtago@gmail.com>
2025-12-10 20:08:44 +0800

    added 'Add to Cart' functions with total computation

--- src/components/menu/BillInfo.jsx
+++ src/components/menu/BillInfo.jsx
@@ -1,30 +1,73 @@
-import React from 'react'
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getTotalPrice, removeAllItems } from "../../redux/slices/cartSlice";
+import { removeCustomer } from "../../redux/slices/customerSlice";
 
-const BillInfo = () => {
-  return (
-    <>
-      <div className='flex items-center justify-between px-5 mt-2'>
-        <p className='text-xs text-gray-600 font-medium mt-2'>Items(6)</p>
-        <h1 className='text-gray-800 text-md font-bold'>₱1,500.00</h1>
-      </div>
-      <div className='flex items-center justify-between px-5 mt-2'>
-        <p className='text-xs text-gray-600 font-medium mt-2'>Service Fee(5.25%)</p>
-        <h1 className='text-gray-800 text-md font-bold'>₱78.75</h1>
-      </div>
-      <div className='flex items-center justify-between px-5 mt-2'>
-        <p className='text-xs text-gray-600 font-bold mt-2'>Total</p>
-        <h1 className='text-gray-800 text-lg font-bold'>₱1,578.75</h1>
-      </div>
-      <div className='flex items-center gap-3 px-5 mt-4'>
-        <button className='bg-gray-200 px-4 py-3 w-full rounded-lg text-gray-800 font-semibold'>Cash</button>
-        <button className='bg-gray-200 px-4 py-3 w-full rounded-lg text-gray-800 font-semibold'>Card</button>
-      </div>
-      <div className='flex items-center gap-3 px-5 mt-4'>
-        <button className='bg-[#025cca] px-4 py-3 w-full rounded-lg text-gray-200 font-semibold'>Print Receipt</button>
-        <button className='bg-[#9D5623] px-4 py-3 w-full rounded-lg text-gray-200 font-semibold'>Place Order</button>
-      </div>
-    </>
-  )
+function loadScript(src) {
+  return new Promise((resolve) => {
+    const script = document.createElement("script");
+    script.src = src;
+    script.onload = () => {
+      resolve(true);
+    };
+    script.onerror = () => {
+      resolve(false);
+    };
+    document.body.appendChild(script);
+  });
 }
 
-export default BillInfo
+const BillInfo = () => {
+  const dispatch = useDispatch();
+
+  // Calculate Total Order
+  const customerData = useSelector((state) => state.customer);
+  const cartData = useSelector((state) => state.cart);
+  const total = useSelector(getTotalPrice);
+  const taxRate = 5.25;
+  const tax = (total * taxRate) / 100;
+  const totalPriceWithTax = total + tax;
+
+  const [orderInfo, setOrderInfo] = useState();
+
+  return (
+    <>
+      <div className="flex items-center justify-between px-5 mt-2">
+        <p className="text-xs text-gray-600 font-medium mt-2">
+          Items({cartData.length})
+        </p>
+        <h1 className="text-gray-800 text-md font-bold">₱{total.toFixed(2)}</h1>
+      </div>
+      <div className="flex items-center justify-between px-5 mt-2">
+        <p className="text-xs text-gray-600 font-medium mt-2">
+          Service Fee(5.25%)
+        </p>
+        <h1 className="text-gray-800 text-md font-bold">₱{tax.toFixed(2)}</h1>
+      </div>
+      <div className="flex items-center justify-between px-5 mt-2">
+        <p className="text-xs text-gray-600 font-bold mt-2">Total</p>
+        <h1 className="text-gray-800 text-lg font-bold">
+          ₱{totalPriceWithTax.toFixed(2)}
+        </h1>
+      </div>
+      <div className="flex items-center gap-3 px-5 mt-4">
+        <button className="bg-gray-200 px-4 py-3 w-full rounded-lg text-gray-800 font-semibold">
+          Cash
+        </button>
+        <button className="bg-gray-200 px-4 py-3 w-full rounded-lg text-gray-800 font-semibold">
+          Card
+        </button>
+      </div>
+      <div className="flex items-center gap-3 px-5 mt-4">
+        <button className="bg-[#025cca] px-4 py-3 w-full rounded-lg text-gray-200 font-semibold">
+          Print Receipt
+        </button>
+        <button className="bg-[#9D5623] px-4 py-3 w-full rounded-lg text-gray-200 font-semibold">
+          Place Order
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default BillInfo;
--- src/components/menu/MenuContainer.jsx
+++ src/components/menu/MenuContainer.jsx
@@ -1,104 +1,118 @@
-import { useState } from 'react'
-import { menus } from '../../constants'
-import { GrRadialSelected } from 'react-icons/gr'
-import { FaBowlFood } from "react-icons/fa6";
-import { useDispatch } from 'react-redux';
+import { useState } from "react";
+import { menus } from "../../constants";
+import { GrRadialSelected } from "react-icons/gr";
+import { FaCartArrowDown } from "react-icons/fa6";
+import { useDispatch } from "react-redux";
+import { addItems } from "../../redux/slices/cartSlice";
 
 const MenuContainer = () => {
+  const [selected, setSelected] = useState(menus[0]);
+  const [itemCount, setItemCount] = useState(0);
+  const [itemId, setItemId] = useState();
+  const dispatch = useDispatch();
 
-    const [selected, setSelected] = useState(menus[0]);
-    const [itemCount, setItemCount] = useState(0);
-    const [itemId, setItemId] = useState();
-    const dispatch = useDispatch();
+  const increment = (id) => {
+    setItemId(id);
+    if (itemCount >= 4) return;
+    setItemCount((prev) => prev + 1);
+  };
 
-    const increment = (id) => {
-        setItemId(id);
-        if (itemCount >= 4) return;
-        setItemCount((prev) => prev + 1);
+  const decrement = (id) => {
+    setItemId(id);
+    if (itemCount <= 0) return;
+    setItemCount((prev) => prev - 1);
+  };
+
+  const handleAddToCart = (item) => {
+    if (itemCount === 0) return;
+
+    const { name, price } = item;
+    const newObj = {
+      id: new Date(),
+      name,
+      pricePerQuantity: price,
+      quantity: itemCount,
+      price: price * itemCount,
     };
 
-    const decrement = (id) => {
-        setItemId(id);
-        if (itemCount <= 0) return;
-        setItemCount((prev) => prev - 1);
-    };
+    dispatch(addItems(newObj));
+    setItemCount(0);
+  };
 
-    return (
-        <>
-            <div className='grid grid-cols-4 gap-4 px-10 py-4 w-full'>
-                {
-                    menus.map((menu) => {
-                        return (
-                            <div
-                                key={menu.id}
-                                className='flex flex-col items-start justify-between p-4 rounded-lg h-[100px] cursor-pointer'
-                                style={{ backgroundColor: menu.bgColor }}
-                                onClick={() => {
-                                    setSelected(menu);
-                                    setItemId(0);
-                                    setItemCount(0);
-                                }}
-                            >
-                                <div className='flex items-center justify-between w-full'>
-                                    <h1 className='text-gray-100 text-lg font-semibold'>{menu.icon} {menu.name}</h1>
-                                    {selected.id === menu.id && <GrRadialSelected className='text-gray-200' size={20} />}
-                                </div>
-                                <p className='text-gray-200 text-sm font-semibold'>
-                                    {menu.items.length} Items
-                                </p>
-                            </div>
-                        )
-                    })
-                }
+  return (
+    <>
+      <div className="grid grid-cols-4 gap-4 px-10 py-4 w-full">
+        {menus.map((menu) => {
+          return (
+            <div
+              key={menu.id}
+              className="flex flex-col items-start justify-between p-4 rounded-lg h-[100px] cursor-pointer"
+              style={{ backgroundColor: menu.bgColor }}
+              onClick={() => {
+                setSelected(menu);
+                setItemId(0);
+                setItemCount(0);
+              }}>
+              <div className="flex items-center justify-between w-full">
+                <h1 className="text-gray-100 text-lg font-semibold">
+                  {menu.icon} {menu.name}
+                </h1>
+                {selected.id === menu.id && (
+                  <GrRadialSelected className="text-gray-200" size={20} />
+                )}
+              </div>
+              <p className="text-gray-200 text-sm font-semibold">
+                {menu.items.length} Items
+              </p>
             </div>
+          );
+        })}
+      </div>
 
-            <hr className='border-gray-200 border-t-2 mt-4' />
+      <hr className="border-gray-200 border-t-2 mt-4" />
 
-            <div className='grid grid-cols-4 gap-4 px-10 py-4 w-full'>
-                {
-                    selected?.items.map((menu) => {
-                        return (
-                            <div
-                                key={menu.id}
-                                className='flex flex-col items-start justify-between p-4 rounded-lg h-[150px] cursor-pointer hover:bg-stone-200 bg-gray-200'
-                            >
-                                <div className='flex items-start justify-between w-full'>
-                                    <h1 className='text-gray-900 text-lg font-semibold'>
-                                        {menu.name}
-                                    </h1>
-                                    <button className='text-green-800 bg-[#8aceb5] p-2 rounded-lg cursor-pointer'>
-                                        <FaBowlFood size={20} />
-                                    </button>
-                                </div>
-                                <div className='flex items-center justify-between w-full'>
-                                    <p className='text-gray-800 text-xl font-semibold'>
-                                        ₱{menu.price}
-                                    </p>
-                                    <div className="flex items-center justify-between px-4 py-3 rounded-lg gap-6 w-[50%]">
-                                        <button
-                                            onClick={() => decrement(menu.id)}
-                                            className="text-[#9D5623] text-2xl"
-                                        >
-                                            &minus;
-                                        </button>
-                                        <span className="text-gray-800">
-                                            {menu.id == itemId ? itemCount : "0"}
-                                        </span>
-                                        <button
-                                            onClick={() => increment(menu.id)}
-                                            className="text-[#9D5623] text-2xl"
-                                        >
-                                            &#43;
-                                        </button>
-                                    </div>
-                                </div>
-                            </div>
-                        )
-                    })
-                }
+      <div className="grid grid-cols-4 gap-4 px-10 py-4 w-full">
+        {selected?.items.map((item) => {
+          return (
+            <div
+              key={item.id}
+              className="flex flex-col items-start justify-between p-4 rounded-lg h-[150px] cursor-pointer hover:bg-stone-200 bg-gray-200">
+              <div className="flex items-start justify-between w-full">
+                <h1 className="text-gray-900 text-lg font-semibold">
+                  {item.name}
+                </h1>
+                <button
+                  onClick={() => handleAddToCart(item)}
+                  className="text-green-800 bg-[#8aceb5] p-2 rounded-lg cursor-pointer">
+                  <FaCartArrowDown size={20} />
+                </button>
+              </div>
+              <div className="flex items-center justify-between w-full">
+                <p className="text-gray-800 text-xl font-semibold">
+                  ₱{item.price}
+                </p>
+                <div className="flex items-center justify-between px-4 py-3 rounded-lg gap-6 w-[50%]">
+                  <button
+                    onClick={() => decrement(item.id)}
+                    className="text-[#9D5623] text-2xl">
+                    &minus;
+                  </button>
+                  <span className="text-gray-800">
+                    {item.id == itemId ? itemCount : "0"}
+                  </span>
+                  <button
+                    onClick={() => increment(item.id)}
+                    className="text-[#9D5623] text-2xl">
+                    &#43;
+                  </button>
+                </div>
+              </div>
             </div>
-        </>
-    )
-}
+          );
+        })}
+      </div>
+    </>
+  );
+};
 
-export default MenuContainer
+export default MenuContainer;
