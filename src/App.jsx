c33a1b93602ca112d7e4384b7eca8645e09cb2b2
anthonytagorda <anthtago@gmail.com>
2025-12-11 12:34:48 +0800

    moved Microsoft Clarity initialization

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,20 @@
-import { useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Home, Auth, Orders, Tables, Menu } from "./pages";
 import Header from "./components/shared/Header";
-import Clarity from "@microsoft/clarity";
 
 function App() {
-  // Microsoft Clarity
-  useEffect(() => {
-    Clarity.init("ujo59gd873");
-  }, []);
-
   return (
-    <>
-      <Router>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/auth" element={<Auth />} />
-          <Route path="/orders" element={<Orders />} />
-          <Route path="/tables" element={<Tables />} />
-          <Route path="/menu" element={<Menu />} />
-          <Route path="*" element={<div>Not Found</div>} />
-        </Routes>
-      </Router>
-    </>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/auth" element={<Auth />} />
+        <Route path="/orders" element={<Orders />} />
+        <Route path="/tables" element={<Tables />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="*" element={<div>Not Found</div>} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,18 @@
-import { StrictMode } from 'react';
-import { Provider } from 'react-redux';
+import { StrictMode } from "react";
+import { Provider } from "react-redux";
 import { createRoot } from "react-dom/client";
-import App from './App'
+import Clarity from "@microsoft/clarity";
+import App from "./App";
 import store from "./redux/store.js";
-import './index.css'
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
-    <StrictMode>
-        <Provider store={store}>
-            <App />
-        </Provider>
-    </StrictMode>
-)
+// Microsoft Clarity
+Clarity.init("ujo59gd873");
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </StrictMode>
+);
