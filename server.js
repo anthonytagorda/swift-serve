dbd9f88e6405a29f50d23259da101cc4c8badc26
Anthony Tagorda <anthtago@gmail.com>
2025-12-23 08:04:29 +0800

    fixed user routes

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import express from "express";
 import connectDB from "./config/database.js";
 import config from "./config/config.js";
 import globalErrorHandler from "./middleware/globalErrorHandler.js";
+import userRoutes from "./routes/routes.js";
 
 dotenv.config();
 
@@ -11,26 +12,29 @@ const server = express();
 // Middlewares
 server.use(express.json());
 
-// Routes
+// Root Endpoint
 server.get("/", (req, res) => {
-    res.send("Server is running!");
+  res.send("Server is running!");
 });
+
+// Other Endpoints
+server.use("/api/user", userRoutes);
 
 // Global Error Handler (must be AFTER routes)
 server.use(globalErrorHandler);
 
 // Start server AFTER DB connection
 const startServer = async () => {
-    try {
-        await connectDB();
+  try {
+    await connectDB();
 
-        server.listen(config.port, () => {
-            console.log(`Server running on port ${config.port}`);
-        });
-    } catch (error) {
-        console.error("Failed to start server:", error.message);
-        process.exit(1);
-    }
+    server.listen(config.port, () => {
+      console.log(`Server running on port ${config.port}`);
+    });
+  } catch (error) {
+    console.error("Failed to start server:", error.message);
+    process.exit(1);
+  }
 };
 
 startServer();
