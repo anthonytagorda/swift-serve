f741086d81ab1bb8a12bf09a5148b1d84052cf4d
Anthony Tagorda <anthtago@gmail.com>
2025-12-25 20:50:02 +0800

    added token verification middleware

---
+++ middleware/token.js
@@ -0,0 +1,26 @@
+import createHttpError from "http-errors";
+import jwt from "jsonwebtoken";
+import config from "../config/config.js";
+import User from "../models/model.js";
+
+export const isVerifiedUser = async (req, res, next) => {
+  try {
+    const { accessToken } = req.cookies;
+
+    if (!accessToken) {
+      return next(createHttpError(401, "Please provide token!"));
+    }
+
+    const decoded = jwt.verify(accessToken, config.accessTokenSecret);
+
+    const user = await User.findById(decoded.id);
+    if (!user) {
+      return next(createHttpError(401, "User does not exist"));
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    next(createHttpError(401, "Invalid or expired token"));
+  }
+};
