6b2e924d6261032025bc7d7eb74fc14ed5562324
Anthony Tagorda <anthtago@gmail.com>
2025-12-25 20:49:39 +0800

    added JWT secret config

--- config/config.js
+++ config/config.js
@@ -2,11 +2,16 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+if (!process.env.JWT_SECRET) {
+  throw new Error("JWT_SECRET is missing in environment variables");
+}
+
 const config = Object.freeze({
-    port: process.env.PORT || 3000,
-    databaseURI:
-        process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/swiftserve",
-    nodeEnv: process.env.NODE_ENV || "development",
+  port: process.env.PORT || 3000,
+  databaseURI:
+    process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/swiftserve",
+  nodeEnv: process.env.NODE_ENV || "development",
+  accessTokenSecret: process.env.JWT_SECRET,
 });
 
 export default config;
