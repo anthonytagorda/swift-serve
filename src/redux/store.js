58334a3e29b553d92e9d5f27b08cd3277cc74077
Anthony Tagorda <anthtago@gmail.com>
2026-01-02 11:35:26 +0800

    added userSlice

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import customerReducer from "./slices/customerSlice";
 import cartSlice from "./slices/cartSlice";
+import userSlice from "./slices/userSlice";
 
 const store = configureStore({
   reducer: {
     customer: customerReducer,
     cart: cartSlice,
+    user: userSlice,
   },
   devTools: import.meta.env.MODE !== "production",
 });
