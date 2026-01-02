da014e3d3dbc959290ea8510001ed2a21189fcb3
Anthony Tagorda <anthtago@gmail.com>
2026-01-03 00:40:17 +0800

    implemented login flow with react-query, redux, and snackbar

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,7 @@
 import { StrictMode } from "react";
 import { Provider } from "react-redux";
+import { SnackbarProvider } from "notistack";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createRoot } from "react-dom/client";
 import Clarity from "@microsoft/clarity";
 import App from "./App";
@@ -9,10 +11,22 @@ import "./index.css";
 // Microsoft Clarity
 Clarity.init("ujo59gd873");
 
+// Tanstack Query
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 30000,
+    }
+  }
+})
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <SnackbarProvider autoHideDuration={3000}>
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
+      </SnackbarProvider>
     </Provider>
   </StrictMode>
 );
