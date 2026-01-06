46617196e836946f2fa1ff311d0cf5ac0c07f6f3
Anthony Tagorda <anthtago@gmail.com>
2026-01-06 20:52:54 +0800

    updated roles sequence

--- models/userModel.js
+++ models/userModel.js
@@ -2,43 +2,43 @@ import mongoose from "mongoose";
 import bcrypt from "bcrypt";
 
 const userSchema = new mongoose.Schema(
-  {
-    name: { type: String, required: true },
+	{
+		name: { type: String, required: true },
 
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-      match: [/^\S+@\S+\.\S+$/, "Email must be valid"],
-    },
+		email: {
+			type: String,
+			required: true,
+			unique: true,
+			match: [/^\S+@\S+\.\S+$/, "Email must be valid"],
+		},
 
-    phone: {
-      type: String,
-      required: true,
-      match: [/^\d{11}$/, "Phone number must be 11 digits"],
-    },
+		phone: {
+			type: String,
+			required: true,
+			match: [/^\d{11}$/, "Phone number must be 11 digits"],
+		},
 
-    password: {
-      type: String,
-      required: true,
-    },
+		password: {
+			type: String,
+			required: true,
+		},
 
-    role: {
-      type: String,
-      required: true,
-      enum: ["Manager", "Cashier", "Waiter"],
-    },
-  },
-  { timestamps: true }
+		role: {
+			type: String,
+			required: true,
+			enum: ["Waiter", "Cashier", "Manager"],
+		},
+	},
+	{ timestamps: true },
 );
 
 userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) {
-    next();
-  }
+	if (!this.isModified("password")) {
+		next();
+	}
 
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
+	const salt = await bcrypt.genSalt(10);
+	this.password = await bcrypt.hash(this.password, salt);
 });
 
 export default mongoose.model("User", userSchema);
